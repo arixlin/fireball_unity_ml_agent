c6037209aa67286444a400480d3d7ce610f365cf
arixlin <linmingzxx@gmail.com>
2018-04-28 15:51:57 +0800

    Academy

---
+++ Assets/code/FireballAcademy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballAcademy : Academy {
+
+    [HideInInspector]
+    public GameObject[] agents;
+
+    public override void AcademyReset()
+    {
+        base.AcademyReset();
+        //ClearObjects(GameObject.FindGameObjectsWithTag("food"));
+        //ClearObjects(GameObject.FindGameObjectsWithTag("badfood"));
+
+        //agents = GameObject.FindGameObjectsWithTag("agent");
+    }
+
+    void ClearObjects(GameObject[] objects)
+    {
+        foreach (GameObject food in objects)
+        {
+            Destroy(food);
+        }
+    }
+}
